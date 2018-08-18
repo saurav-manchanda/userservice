bfe70d8f1f3b4e2b402ea760856b26de78e345a8
saurav-manchanda <saurav.manchanda2009@gmail.com>
2018-08-18 10:38:22 +0530

    user micro services

--- src/main/java/com/bridgelabz/userservice/controller/UserController.java
+++ src/main/java/com/bridgelabz/userservice/controller/UserController.java
@@ -19,6 +19,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -45,6 +46,7 @@ import com.bridgelabz.userservice.utilservice.ObjectMapper.ObjectMapping;
  *         and what method to run when accessed.
  *         </p>
  */
+@RefreshScope
 @RestController
 @RequestMapping(value = "/users")
 public class UserController {
